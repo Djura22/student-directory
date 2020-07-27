29fa03db8ff363c1cecd484f8014cae1f559cbe5
Djura22 <michaeliangroves@gmail.com>
2020-07-27 10:44:33 +0000

    comments for directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# 1. print a list of the students
 puts "The students of Villains Academy"
 puts "--------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+# 2. print total of students
 print "Overall, we have "
+# print used here to ensure no line breaks
 print 11
 puts " great students"
