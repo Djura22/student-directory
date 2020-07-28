1eaebcc785ddbff795bbca0144613e5bbb168438
Djura22 <michaeliangroves@gmail.com>
2020-07-28 10:37:17 +0000

    index added to student list

--- directory.rb
+++ directory.rb
@@ -41,8 +41,8 @@ def print_header
 end
 # 3. print the students names from within the array
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 # 4. print the total of the students
