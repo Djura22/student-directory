307ea972a61b3d8ebfed1f9426bdfd36038b5a59
Djura22 <michaeliangroves@gmail.com>
2020-07-29 12:53:46 +0000

    custom cohort and user conf

--- directory.rb
+++ directory.rb
@@ -9,17 +9,30 @@ def input_students
   puts "Leave blank if you wish to finish"
   # creation of empty array
   students = []
-  name = gets.chomp
+  name = gets.chomp.to_sym
   # repeat request until name is empty
   while !name.empty? do
+    puts "Please enter which cohort the student is a member of. i.e July"
+    cohort = gets.chomp.to_sym
+    puts "Is the following correct - Name: #{name}, Cohort: #{cohort} ? type y/n"
+    user_conf = gets.chomp
+    if user_conf == "n"
+      puts "re-enter name"
+      name = gets.chomp.to_sym
+      puts "re-enter cohort (if not known, leave blank)"
+      cohort = gets.chomp.to_sym
+    end
+    if cohort.empty?
+      cohort = "N/A".to_sym
+    end
     puts "Please enter students nationality"
-    nationality = gets.chomp
+    nationality = gets.chomp.to_sym
     puts "Please enter students favourite hobbie"
-    fav_hobbie = gets.chomp
+    fav_hobbie = gets.chomp.to_sym
     puts "Please enter students height in feet, i.e 5.8"
-    height = gets.chomp
+    height = gets.chomp.to_sym
     # add the student hash to the array
-    students << {name: name, nationality: nationality, hobbie: fav_hobbie, height: height, cohort: :november}
+    students << {name: name, nationality: nationality, hobbie: fav_hobbie, height: height, cohort: cohort}
     puts "Now we have #{students.count} students"
     # get more names from the user
     puts "Please enter the name of the next student"
