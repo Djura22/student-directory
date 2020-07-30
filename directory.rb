1275a8a7cec662b403f128362713f9993c7daa29
Djura22 <michaeliangroves@gmail.com>
2020-07-30 17:28:26 +0000

    Refactored #1

--- directory.rb
+++ directory.rb
@@ -1,3 +1,6 @@
+
+@students = []
+
 # creation of class method for filtering by Initial.
 class String
   def initial
@@ -8,8 +11,6 @@ end
 def input_students
   puts "Please enter the name of the student"
   puts "Leave blank if you wish to finish"
-  # creation of empty array
-  students = []
   # asking user for input
   name = gets.chop
   # repeat request until name is empty
@@ -37,91 +38,91 @@ def input_students
     puts "Please enter students height in feet, i.e 5.8"
     height = gets.chop.to_sym
     # add the student hash to the array
-    students << {name: name, nationality: nationality, hobbie: fav_hobbie, height: height, cohort: cohort}
+    @students << {name: name, nationality: nationality, hobbie: fav_hobbie, height: height, cohort: cohort}
     # custom feedback based on student count
-    if students.count > 1
-      puts "Now we have #{students.count} students"
+    if @students.count > 1
+      puts "Now we have #{@students.count} students"
     else
-      puts "Now we have #{students.count} student"
+      puts "Now we have #{@students.count} student"
     end
     # get more names from the user
     puts "Please enter the name of the next student"
     puts "Leave blank if you wish to finish"
     name = gets.chop
   end
-  # return the array of students
-  students
 end
-#  print the header
-def print_header(students)
-  # if no students present, does not run
-  if !students.empty?
-    puts "The Students of Villains Academy".center(88)
-    puts "--------------".center(88)
-  end
-end
-#  print the students names from within the array - with index
-def print(students)
-  while !students.empty? do
-    students.each.with_index(1) do |student, index|
-      puts "#{index}. #{student[:name]}, Nationality: #{student[:nationality]}, Favourite Hobbie: #{student[:hobbie]}, Height(ft): #{student[:height]} (#{student[:cohort]} cohort)"
-    end
-  break
-  end
-end
-# print students by first initial specified
-def print_by_initial(students)
-  while !students.empty? do
-    puts "Please select initial to search by"
-    ini = gets.chomp
-    students.each.with_index(1) do |student, index|
-      if student[:name].upcase.initial == ini.upcase
-        puts "#{index}. #{student[:name]}, Nationality: #{student[:nationality]}, Favourite Hobbie: #{student[:hobbie]}, Height(ft): #{student[:height]} (#{student[:cohort]} cohort)"
-      end
-    end
-    break
-  end
-end
-# print students by specified length range (1-i)
-def print_by_length(students)
-  while !students.empty? do
-  puts "Please select your maximum name length"
-    len = gets.chomp
-    students.each.with_index(1) do |student, index|
-      if student[:name].length <= len.to_i
-        puts "#{index}. #{student[:name]}, Nationality: #{student[:nationality]}, Favourite Hobbie: #{student[:hobbie]}, Height(ft): #{student[:height]} (#{student[:cohort]} cohort)"
-      end
-    end
-    break
-  end
-end
-#  print the total of the students
-def print_footer(names)
-  # customised footer based on student count
-  if names.count > 1
-    puts "Overall, we have #{names.count} great students".center(90)
-  elsif names.count == 1
-    puts "We have #{names.count} great student".center(90)
-  else
-    puts "We have no students".center(90)
-  end
-end
+
 # printing students sorted by cohort
-def print_by_cohort(students)
-  sort_by_cohort = students.map {|student| student[:cohort]}.uniq
+def print_by_cohort
+  sort_by_cohort = @students.map {|student| student[:cohort]}.uniq
   sort_by_cohort.each do |cohort|
-    students.each.with_index(1) do |student, index|
+    @students.each.with_index(1) do |student, index|
       if student[:cohort] == cohort
-        puts "#{index}. #{student[:name]}, Nationality: #{student[:nationality]}, Favourite Hobbie: #{student[:hobbie]}, Height(ft): #{student[:height]} (#{student[:cohort]} cohort)"
+        puts "--- #{index}. #{student[:name]}, Nationality: #{student[:nationality]}, Favourite Hobbie: #{student[:hobbie]}, Height(ft): #{student[:height]} (#{student[:cohort]} cohort)"
       end
     end
   end
 end
 
-def interactive_menu
-  students = []
-  loop do
-  # 1. print the menu and ask the user what to do
+# print students by first initial specified
+def print_by_initial
+  while !@students.empty? do
+    puts "Please select initial to search by"
+    ini = gets.chomp
+    @students.each.with_index(1) do |student, index|
+      if student[:name].upcase.initial == ini.upcase
+        puts "--- #{index}. #{student[:name]}, Nationality: #{student[:nationality]}, Favourite Hobbie: #{student[:hobbie]}, Height(ft): #{student[:height]} (#{student[:cohort]} cohort)"
+      end
+    end
+    break
+  end
+end
+
+# print students by specified length range (1-i)
+def print_by_length
+  while !@students.empty? do
+  puts "Please select your maximum name length"
+    len = gets.chomp
+    @students.each.with_index(1) do |student, index|
+      if student[:name].length <= len.to_i
+        puts "--- #{index}. #{student[:name]}, Nationality: #{student[:nationality]}, Favourite Hobbie: #{student[:hobbie]}, Height(ft): #{student[:height]} (#{student[:cohort]} cohort)"
+      end
+    end
+    break
+  end
+end
+
+def print_header
+  # if no students present, does not run
+  if !@students.empty?
+    puts "The Students of Villains Academy".center(88)
+    puts "--------------".center(88)
+  end
+end
+
+#  print the students names from within the array - with index
+def print_student_list
+  while !@students.empty? do
+    @students.each.with_index(1) do |student, index|
+      puts "--- #{index}. #{student[:name]}, Nationality: #{student[:nationality]}, Favourite Hobbie: #{student[:hobbie]}, Height(ft): #{student[:height]} (#{student[:cohort]} cohort)"
+    end
+  break
+  end
+end
+
+def print_footer
+  # customised footer based on student count
+  if @students.count > 1
+    puts "Overall, we have #{@students.count} great students".center(90)
+  elsif @students.count == 1
+    puts "We have #{@students.count} great student".center(90)
+  else
+    puts "We have no students".center(90)
+  end
+end
+
+# menu options
+def print_menu
   puts "Welcome to the Student Directory, how would you like to proceed?"
   puts "1. Input new students"
   puts "2. Print student list"
@@ -129,31 +130,44 @@ def interactive_menu
   puts "4. Print list of students with specific initial"
   puts "5. Print list of students with name length filter (i.e 1-10)"
   puts "9. To terminate program"
+end
+
+# prints students with no filters
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+# call to the menu and receiving users input
+def interactive_menu
+  loop do
+  # 1. print the menu and ask the user what to do
+    print_menu
   # 2. read the input and save it into a variable
-  selection = gets.chomp
-  # 3. do what the user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header(students)
-        print(students)
-        print_footer(students)
-      when "3"
-        print_by_cohort(students)
-      when "4"
-        print_by_initial(students)
-      when "5"
-        print_by_length(students)
-      when "9"
-        exit
-      else
-        puts "Invalid choice, please select again from numbers provided."
-    end
+    process(gets.chomp)
   end
 end
   
+def process(selection)
+  # do what the user has asked
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      print_by_cohort
+    when "4"
+      print_by_initial
+    when "5"
+      print_by_length
+    when "9"
+      exit
+    else
+      puts "Invalid choice, please select again from numbers provided."
+  end
+end
   
-  
-# call methods
+# call menu (run program)
 interactive_menu
