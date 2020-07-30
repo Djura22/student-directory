6003042a632e9cfd33cd83c5aa6f1982c9f82573
Djura22 <michaeliangroves@gmail.com>
2020-07-30 13:21:17 +0000

    Chop method used in place of chomp

--- directory.rb
+++ directory.rb
@@ -10,28 +10,28 @@ def input_students
   puts "Leave blank if you wish to finish"
   # creation of empty array
   students = []
-  name = gets.chomp.to_sym
+  name = gets.chop.to_sym
   # repeat request until name is empty
   while !name.empty? do
     puts "Please enter which cohort the student is a member of. i.e July"
-    cohort = gets.chomp.to_sym
+    cohort = gets.chop.to_sym
     puts "Is the following correct - Name: #{name}, Cohort: #{cohort} ? type y/n"
-    user_conf = gets.chomp
+    user_conf = gets.chop
     if user_conf == "n"
       puts "re-enter name"
-      name = gets.chomp.to_sym
+      name = gets.chop.to_sym
       puts "re-enter cohort (if not known, leave blank)"
-      cohort = gets.chomp.to_sym
+      cohort = gets.chop.to_sym
     end
     if cohort.empty?
       cohort = "N/A".to_sym
     end
     puts "Please enter students nationality"
-    nationality = gets.chomp.to_sym
+    nationality = gets.chop.to_sym
     puts "Please enter students favourite hobbie"
-    fav_hobbie = gets.chomp.to_sym
+    fav_hobbie = gets.chop.to_sym
     puts "Please enter students height in feet, i.e 5.8"
-    height = gets.chomp.to_sym
+    height = gets.chop.to_sym
     # add the student hash to the array
     students << {name: name, nationality: nationality, hobbie: fav_hobbie, height: height, cohort: cohort}
     if students.count > 1
@@ -42,7 +42,7 @@ def input_students
     # get more names from the user
     puts "Please enter the name of the next student"
     puts "Leave blank if you wish to finish"
-    name = gets.chomp
+    name = gets.chop
   end
   # return the array of students
   students
